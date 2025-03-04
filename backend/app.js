324cf730921f58531f64caa186d41a066e637f5e
Marc Frances <marc.frances.t@gmail.com>
2025-03-04 11:08:46 +0100

    Add health check path

--- backend/app.js
+++ backend/app.js
@@ -38,6 +38,10 @@ app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 if (process.env.NODE_ENV === 'test') {
   app.use('/api/testing', testingRouter)
 }
