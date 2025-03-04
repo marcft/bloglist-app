65259893775790c4d0326dc6e56dd9f0f152ce9f
Marc Frances <marc.frances.t@gmail.com>
2025-03-04 12:51:36 +0100

    Don't log the successful health checks

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,7 +1,14 @@
 const info = (...params) => {
-  if (process.env.NODE_ENV !== 'test') {
-    console.log(...params)
-  }
+  if (process.env.NODE_ENV === 'test') return
+  if (
+    params[0] &&
+    typeof params[0] === 'string' &&
+    (params[0].includes('GET /health 200') ||
+      params[0].includes('GET /health 304'))
+  )
+    return
+
+  console.log(...params)
 }
 
 const error = (...params) => {
